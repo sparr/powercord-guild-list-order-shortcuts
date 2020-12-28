4b0c73f92f1c03023417d011f6d49b368f85000a
Clarence "Sparr" Risher <sparr0@gmail.com>
2020-12-28 14:50:57 -0800

    Deduplicate code, add unnamed folder labels, remove dashName, improve alphabetizing, add comments

--- index.js
+++ index.js
@@ -34,54 +34,14 @@ module.exports = class GuildListOrderShortcuts extends Plugin {
                   id: 'guild-list-order-shortcuts-move-guild-to-top',
                   key: 'guild-list-order-shortcuts-move-guild-to-top',
                   label: Messages.TO_TOP,
-                  action: () => this._reorderGuilds((guildFolders) => {
-                    findGuild:
-                    // loop over all the folders, including fake one-guild folders
-                    for(var folderIndex = 0; folderIndex < guildFolders.length; folderIndex++){
-                      const folder = guildFolders[folderIndex];
-                      // loop over every guild within a folder to find the selected guild
-                      for(var guildIndex = 0; guildIndex < folder.guildIds.length; guildIndex++){
-                        if (folder.guildIds[guildIndex] === guild.id) {
-                          // remove this guild from the folder
-                          folder.guildIds.splice(guildIndex, 1);
-                          if (!folder.hasOwnProperty('folderId') && folder.guildIds.length == 0) {
-                            // remove empty fake folders from the list
-                            guildFolders.splice(folderIndex, 1);
-                          }
-                          // put this guild at the top in a new fake folder
-                          guildFolders.unshift({guildIds: [guild.id]});
-                          break findGuild;
-                        }
-                      }
-                    }
-                  })
+                  action: () => this._moveSingleGuildById(guild.id, (guildFolders, guildId) => guildFolders.unshift({guildIds: [guildId]}))
                 })
                 ,
                 React.createElement(MenuItem, {
                   id: 'guild-list-order-shortcuts-move-guild-to-bottom',
                   key: 'guild-list-order-shortcuts-move-guild-to-bottom',
                   label: Messages.TO_BOTTOM,
-                  action: () => this._reorderGuilds((guildFolders) => {
-                    findGuild:
-                    // loop over all the folders, including fake one-guild folders
-                    for(var folderIndex = 0; folderIndex < guildFolders.length; folderIndex++){
-                      const folder = guildFolders[folderIndex];
-                      // loop over every guild within a folder to find the selected guild
-                      for(var guildIndex = 0; guildIndex < folder.guildIds.length; guildIndex++){
-                        if (folder.guildIds[guildIndex] === guild.id) {
-                          // remove this guild from the folder
-                          folder.guildIds.splice(guildIndex, 1) ;
-                          if (!folder.hasOwnProperty('folderId') && folder.guildIds.length == 0) {
-                            // remove empty fake folders from the list
-                            guildFolders.splice(folderIndex, 1);
-                          }
-                          // put this guild at the bottom in a new fake folder
-                          guildFolders.push({guildIds: [guild.id]});
-                          break findGuild
-                        }
-                      }
-                    }
-                  })
+                  action: () => this._moveSingleGuildById(guild.id, (guildFolders, guildId) => guildFolders.push({guildIds: [guildId]}))
                 })
                 ,
                 React.createElement(MenuItem, {
@@ -95,36 +55,14 @@ module.exports = class GuildListOrderShortcuts extends Plugin {
                     if ('folderName' in targetFolder) {
                       folderLabel = targetFolder.folderName;
                     } else {
-                      //TODO replicate the client behavior of using "Server Name 1, Server Name 2, ..." as the name for no-name folders
-                      folderLabel = targetFolder.folderId.toString();
+                      folderLabel = getGuild(targetFolder.guildIds[0])?.name + ', ...';
                     }
-                    const dashName = folderLabel.replace(/[^0-9a-z]/gi, '-');
                     // build the menu item for this target folder
                     return React.createElement(MenuItem, {
-                      id: 'guild-list-order-shortcuts-move-guild-to-folder-' + dashName,
-                      key: 'guild-list-order-shortcuts-move-guild-to-folder-' + dashName,
+                      id: 'guild-list-order-shortcuts-move-guild-to-folder-' + targetFolder.folderId,
+                      key: 'guild-list-order-shortcuts-move-guild-to-folder-' + targetFolder.folderId,
                       label: folderLabel,
-                      action: () => this._reorderGuilds((guildFolders) => {
-                        findGuild:
-                        // loop over all the folders, including fake one-guild folders
-                        for(var folderIndex = 0; folderIndex < guildFolders.length; folderIndex++){
-                          const folder = guildFolders[folderIndex]
-                          // loop over every guild within a folder to find the selected guild
-                          for(var guildIndex = 0; guildIndex < folder.guildIds.length; guildIndex++){
-                            if (folder.guildIds[guildIndex] === guild.id) {
-                              // remove this guild from the folder
-                              folder.guildIds.splice(guildIndex, 1)
-                              if (!folder.hasOwnProperty('folderId') && folder.guildIds.length == 0) {
-                                // remove empty fake folders from the list
-                                guildFolders.splice(folderIndex, 1)
-                              }
-                              // put this guild in the target folder
-                              guildFolders[targetFolderIndex].guildIds.push(guild.id);
-                              break findGuild
-                            }
-                          }
-                        }
-                      })
+                      action: () => this._moveSingleGuildById(guild.id, (guildFolders, guildId) => guildFolders[targetFolderIndex].guildIds.push(guild.id))
                     })
                   }).filter(e => e)
                 })
@@ -140,77 +78,30 @@ module.exports = class GuildListOrderShortcuts extends Plugin {
                   id: 'guild-list-order-shortcuts-sort-servers-alphabetically',
                   key: 'guild-list-order-shortcuts-sort-servers-alphabetically',
                   label: Messages.ALPHABETICALLY,
-                  action: () => this._reorderGuilds((guildFolders) => {
-                    guildFolders.sort((a,b) => {
-                      // real folders have a folderId and get sorted to the bottom
-                      if ('folderId' in a) {
-                        if ('folderId' in b) {
-                          return 0;
-                        }
-                        return 1;
-                      }
-                      if ('folderId' in b) {
-                        return -1;
-                      }
-                      // ?. because users can somehow end up with servers in their guildFolders list but not actually be in them?
-                      // toUpperCase to make the sort case insensitive
-                      const aName = getGuild(a.guildIds[0])?.name.toUpperCase();
-                      const bName = getGuild(b.guildIds[0])?.name.toUpperCase();
-                      // normal alphabetic sorting
-                      if (aName < bName) return -1;
-                      if (aName > bName) return 1;
-                      return 0;
-                    })
-                  })
+                  // guild name, convert non-letters/numbers to spaces, trim leading/trailing spaces, case insensitive
+                  action: () => this._sortGuildFolders(guildId => getGuild(guildId)?.name.replace(/[^\p{L}\p{N}]/gu, ' ').trim().toUpperCase())
                 })
                 ,
                 React.createElement(MenuItem, {
                   id: 'guild-list-order-shortcuts-sort-servers-by-join-date',
                   key: 'guild-list-order-shortcuts-sort-servers-by-join-date',
                   label: Messages.BY_JOIN_DATE,
-                  action: () => this._reorderGuilds((guildFolders) => {
-                    guildFolders.sort((a,b) => {
-                      if (a.guildIds.length > 1) {
-                        if (b.guildIds.length > 1) {
-                          return 0;
-                        }
-                        return 1;
-                      }
-                      const aDate = getGuild(a.guildIds[0])?.joinedAt;
-                      const bDate = getGuild(b.guildIds[0])?.joinedAt;
-                      if (aDate < bDate) return -1;
-                      if (aDate > bDate) return 1;
-                      return 0;
-                    })
-                  })
+                  action: () => this._sortGuildFolders(guildId => getGuild(guildId)?.joinedAt)
                 })
                 ,
                 React.createElement(MenuItem, {
                   id: 'guild-list-order-shortcuts-sort-servers-by-creation-date',
                   key: 'guild-list-order-shortcuts-sort-servers-by-creation-date',
                   label: Messages.BY_CREATION_DATE,
-                  action: () => this._reorderGuilds((guildFolders) => {
-                    guildFolders.sort((a,b) => {
-                      if (a.guildIds.length > 1) {
-                        if (b.guildIds.length > 1) {
-                          return 0;
-                        }
-                        return 1;
-                      }
-                      const aDate = extractTimestamp(a.guildIds[0]);
-                      const bDate = extractTimestamp(b.guildIds[0]);
-                      if (aDate < bDate) return -1;
-                      if (aDate > bDate) return 1;
-                      return 0;
-                    })
-                  })
+                  action: () => this._sortGuildFolders(guildId => extractTimestamp(guildId))
                 })
                 ,
                 React.createElement(MenuItem, {
                   id: 'guild-list-order-shortcuts-sort-servers-randomly',
                   key: 'guild-list-order-shortcuts-sort-servers-randomly',
                   label: Messages.RANDOMLY,
-                  action: () => this._reorderGuilds((guildFolders) => {
+                  action: () => this._reorderGuildFolders((guildFolders) => {
+                    // Durstenfeld shuffle from https://stackoverflow.com/a/12646864/13675
                     for (let i = guildFolders.length - 1; i > 0; i--) {
                       const j = Math.floor(Math.random() * (i + 1));
                       [guildFolders[i], guildFolders[j]] = [guildFolders[j], guildFolders[i]];
@@ -231,14 +122,74 @@ module.exports = class GuildListOrderShortcuts extends Plugin {
     GuildContextMenu.default.displayName = 'GuildContextMenu';
   }
 
-  _reorderGuilds(reorderFunc) {
+  /**
+   * Reorder the Guild list using a callback
+   * @param {callback} guildFoldersReorderFunc - Function that reorders a guildFolders array in place
+   */  
+  _reorderGuildFolders(guildFoldersReorderFunc) {
     const { guildFolders } = getModule([ 'guildFolders' ], false);
     // copy necessary? encountered mysterious failures to update later after attempt to modify and re-use the existing array
     var newGuildFolders = [ ...guildFolders ]
     // apply the given reordering function
-    reorderFunc(newGuildFolders)
+    guildFoldersReorderFunc(newGuildFolders)
     // make the client and server aware of the new guildFolders structure
     getModule([ 'updateRemoteSettings' ], false).updateRemoteSettings({ guildFolders: newGuildFolders });
+  }
+
+  /**
+   * Reorder the Guild list, moving a single guild to a new location
+   * @param {string} guildId - numeric ID of the guild as a string
+   * @param {callback} pasteFunc - Function that puts the guild in its new location after it has been "cut"
+   */  
+  _moveSingleGuildById(guildId, pasteFunc) {
+    this._reorderGuildFolders((guildFolders) => {
+      findGuild:
+      // loop over all the folders, including fake one-guild folders
+      for(var folderIndex = 0; folderIndex < guildFolders.length; folderIndex++){
+        const folder = guildFolders[folderIndex];
+        // loop over every guild within a folder to find the selected guild
+        for(var guildIndex = 0; guildIndex < folder.guildIds.length; guildIndex++){
+          if (folder.guildIds[guildIndex] === guildId) {
+            // remove this guild from the folder
+            folder.guildIds.splice(guildIndex, 1) ;
+            // put the guild in its new location
+            pasteFunc(guildFolders, guildId);
+            if (!folder.hasOwnProperty('folderId') && folder.guildIds.length == 0) {
+              // remove now-empty fake folder from the list
+              guildFolders.splice(folderIndex, 1);
+            }
+            break findGuild
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Sort the guild list according to some key function
+   * @param {callback} guildIdKeyFunc - Function that exctracts a key for a guild based on its id
+   */  
+  _sortGuildFolders(guildIdKeyFunc) {
+    this._reorderGuildFolders((guildFolders) => {
+      guildFolders.sort((a,b) => {
+        // real folders have a folderId and get sorted to the bottom
+        if ('folderId' in a) {
+          if ('folderId' in b) {
+            return 0;
+          }
+          return 1;
+        }
+        if ('folderId' in b) {
+          return -1;
+        }
+        const aCmp = guildIdKeyFunc(a.guildIds[0]);
+        const bCmp = guildIdKeyFunc(b.guildIds[0]);
+        console.log(aCmp, bCmp);
+        if (aCmp < bCmp) return -1;
+        if (aCmp > bCmp) return 1;
+        return 0;
+      })
+    });
   }
 
   pluginWillUnload() {
